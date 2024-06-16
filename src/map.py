c7924b95658c99b32343762a7483278499597b24
zaklance <wosewick@gmail.com>
2024-06-16 11:12:29 -0400

    deleted duplicate building class in map

--- src/map.py
+++ src/map.py
@@ -19,17 +19,6 @@ class Building(pygame.sprite.Sprite):
         screen.blit(self.image, self.rect)
 
 
-class Building(pygame.sprite.Sprite):
-    def __init__(self, building, x, y, flip = False):
-        self.image = pygame.image.load(f'assets/img/buildings/{building}.png')
-        if flip:
-            self.image = pygame.transform.flip(self.image, True, False)
-        self.rect = self.image.get_rect()
-        self.rect.bottomleft = (x, y)
-
-    def draw(self, screen):
-        screen.blit(self.image, self.rect)
-
 def update_bg_scroll(bg_scroll, screen_scroll, bg_width, bg_height, screen_width, screen_height):
     if bg_scroll is None:
         bg_scroll = [0, 0]  # Initialize bg_scroll if it's None
